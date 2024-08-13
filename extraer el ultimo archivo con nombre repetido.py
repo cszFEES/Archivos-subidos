d33ddefe7ec0f87d8756516ca58dfc1a90ab3308
Fermín E. Estrada S. <53344062+cszFEES@users.noreply.github.com>
2024-08-13 16:04:07 +0200

    Update extraer el ultimo archivo con nombre repetido.py

--- extraer el ultimo archivo con nombre repetido.py
+++ extraer el ultimo archivo con nombre repetido.py
@@ -1,8 +1,7 @@
+#import shutil
+
 import os
-import shutil
-
-
-folder_path = 'C:\\Users\\994387\\Downloads\\'
+folder_path = 'C:\\Users\\USER\\Downloads\\'
 lista = []
 for filename in os.listdir(folder_path):
     if 'query_export_results' in filename:
@@ -11,17 +10,15 @@ for filename in os.listdir(folder_path):
 lista = sorted(lista, key=len)
 print(lista)
 
+"""    
 csvPath = folder_path + lista[-1]
 
-import pandas as pd
-
 import zipfile
-
 with zipfile.ZipFile(csvPath, 'r') as zip_ref:
     zip_ref.extractall(folder_path)
-
-"""    
-erroresLotesProd = pd.read_csv('C:\\Users\\994387\\Downloads\\Material Information.csv')
+        
+import pandas as pd
+erroresLotesProd = pd.read_csv('C:\\Users\\USER\\Downloads\\FILE.csv')
 print(erroresLotesProd.head())
 
 with zipfile.ZipFile(csvPath) as myzip:
